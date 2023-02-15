de12eb097cf36ed2ae69a22457909ff4ce4655f8
SnaksiXD <aleksin.roskat@gmail.com>
2023-02-15 18:18:52 +0200

    made buttons actually work

--- sk_react_harjotus/src/pages/Buttons.jsx
+++ sk_react_harjotus/src/pages/Buttons.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import {useState} from "react";
 
 export const Buttons = () =>{
+
+    const[count, setCount,] = useState(0);
+    const[count2, setCount2,] = useState(0);
+
+    function countClicks(){
+        setCount(count +1);
+    }
+
+    function resetClicks(){
+        setCount(0)
+        setCount2(0)
+    }
+
+    function countClicks2(){
+        setCount2(count2 +1)
+    }
+
     return(
         <div>
-            <button>aaaa</button>
+            <button onClick={countClicks}>bread given {count} times!</button>
+            <button onClick={countClicks2}>water given {count2} times!</button>
+            <button onClick={resetClicks}>steal food</button>
         </div>
     );
 }
