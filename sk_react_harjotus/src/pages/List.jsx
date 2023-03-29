8485cf1f40bc19e6f77381e240890989e14f6916
SnaksiXD <aleksin.roskat@gmail.com>
2023-03-29 18:18:43 +0300

    korjattiin listat

--- sk_react_harjotus/src/pages/List.jsx
+++ sk_react_harjotus/src/pages/List.jsx
@@ -1,23 +1,50 @@
-import { react } from "react";
+import { React } from "react";
 import {useState} from "react";
+
 
 export const List = ()=>{
     const[content, setContent]= useState([]);
     const[inputValue,setInputValue]= useState('')
+    function clearList(){
+        setContent([]);
+    }
+
+    const removeHandler = (removeIndex)=>{
+        const removeItem = content.filter((item,index)=>{
+            return removeIndex !== index;
+        });
+        setContent(removeItem);
+    }
+
     return(
         <div>
            <div>
                <form onSubmit={(event)=>{
-                event.preventDeafault()
+                event.preventDefault()
                 const temp = [inputValue,...content];
                  setContent(temp);
                  setInputValue('');
             }}>
                    <input type="text"
                     placeholder="Type here..."
-                     value={inputValue} required/>
+                     value={inputValue}
+                     onChange={e => setInputValue(e.target.value)}
+                     required/>
                    <input type="submit" value="Submit"/>
+
+                 {
+                    content.map((item,index)=>(
+                        <ul>
+                            <li>
+                                <h2 key={index}>{item}</h2>
+                                <button onClick={()=>removeHandler(index)}>Remove</button>
+                            </li>
+                        </ul>
+                    ))
+                 }
+
                </form>
+               <button onClick={clearList}>Clear List</button>
            </div>
         </div>
     );
