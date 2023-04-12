a722f3504addbbac0c5c3d90b7ab74c1d0eb9627
SnaksiXD <aleksin.roskat@gmail.com>
2023-04-12 18:18:59 +0300

    tic tac toe progress

--- sk_react_harjotus/src/tictactoe/Board.jsx
+++ sk_react_harjotus/src/tictactoe/Board.jsx
@@ -4,9 +4,21 @@ import { Square } from "./Square";
 export const Board =()=>{
     return(
         <div>
-            <Square/>
-            <Square/>
-            <Square/>
+            <div className="row">
+               <Square className="b-bottom-r"/>
+               <Square className="b-bottom-r"/>
+               <Square className="b-bottom"/>
+            </div>
+            <div className="row">
+               <Square className="b-bottom-r"/>
+               <Square className="b-bottom-r"/>
+               <Square className="b-bottom"/>
+            </div>
+            <div className="row">
+               <Square className="b-right"/>
+               <Square className="b-right"/>
+               <Square/>
+            </div>
         </div>
     );
 }
