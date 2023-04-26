59948850b172f55b69b7a06bb674e487a2185102
SnaksiXD <aleksin.roskat@gmail.com>
2023-04-26 18:17:26 +0300

    AAAAAAAAA

--- sk_react_harjotus/src/tictactoe/Tictactoe.jsx
+++ sk_react_harjotus/src/tictactoe/Tictactoe.jsx
@@ -8,16 +8,39 @@ const initialBoard=['','','','','','','','',''];
 export const TicTacToe =()=>{
     const[gameState, setGameState]= useState(initialBoard);
     const[isXTurn, setIsXTurn] = useState(true);
+
     const onSquareClick =(index)=>{
         let strings = Array.from(gameState);
-        if(strings[index]){
-            return;
+        if(strings[index] !== ''){
+           return;
         }
+        strings[index] = isXTurn ? 'X' : 'O';
+        setGameState(strings);
+        setIsXTurn(!isXTurn);
     }
+
+   const checkWinner = ()=>{
+       const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+       ];
+       for (let i = 0; 1 < lines.length; i++){
+           const [a,b,c] = lines[i];
+           if(gameState[a] && gameState[a] === gameState[b] && gameState[a] === gamestate[c]){return gameState[a]}
+       }
+       return null;
+   }
+
     return(
         <div>
             <h1>Tic-Tac-Toe</h1>
-            <Board gameState={gameState}/>
+            <Board gameState={gameState} onSquareClick={onSquareClick}/>
         </div>
     );
 }
